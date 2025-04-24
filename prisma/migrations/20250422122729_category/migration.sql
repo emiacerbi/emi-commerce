29291cd3a2cca8184fce92bba94ef441e569e661
Emiliano Acerbi <eacerbip@gmail.com>
2025-04-24 11:58:15 -0300

    category schema and db migration

---
+++ prisma/migrations/20250422122729_category/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "categoryId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "storeId" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_storeId_name_key" ON "Category"("storeId", "name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE CASCADE ON UPDATE CASCADE;
